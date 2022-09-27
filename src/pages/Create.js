ad2fa8b8e238fde136b584944269f8a9efbc264a
Your Name <william.gruszka@gmail.com>
2022-09-27 17:26:15 +1000

    created simple form layout

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,42 @@
+/** @jsxImportSource theme-ui */
+
 import {
   Link
 } from "react-router-dom";
 
 function Create() {
-	return (
-		<div id="create">
 
+	const input = {
+		border: "0",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		padding: "12px",
+		gap: "1px",
+		width: "100%",
+		height: "50px",
+		background: "#FFFFFF",
+		boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.1)",
+		borderRadius: "10px",
+	}
+
+	return (
+		<div id="create" sx={{margin: "20px", display: "flex", flexDirection: "column", alignItems: "center"}}>
+			<h1 sx={{width: "100%", textAlign:"left"}}>
+				New Event
+			</h1>
+			<form sx={{marginTop:"20px", width: "100%", display: "flex", flexDirection: "column", gap: "16px"}}>
+					<input sx={input} placeholder="Your Name"></input>
+					<input sx={input} placeholder="Event Title"></input>
+					<input sx={input} style={{flexDirection: "initial"}} type="date" placeholder=""></input>
+					<input sx={input} placeholder="Location"></input>
+			</form>
+			<Link to='/event' sx={{marginTop: "20px"}}>
+				<button className='btn' id='create-event-btn'>
+					Create event!
+				</button>
+			</Link>
 		</div>
 	);
 }
