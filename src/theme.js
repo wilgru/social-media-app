35e9e66440001eaac552b8a0875a944f4c4c551b
Your Name <william.gruszka@gmail.com>
2022-09-27 15:14:04 +1000

    init theme

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export default {
+    fonts: {
+      body: 'Helvetica, sans-serif',
+      heading: 'Helvetica, sans-serif',
+      monospace: 'Menlo, monospace',
+    },
+    colors: {
+      text: '#000',
+      background: '#F6F2FF',
+      primary: '#33e',
+    },
+  }
