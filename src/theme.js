295dd77ad2ae1122b9bd32eb4e5701c3a4824d3d
Your Name <william.gruszka@gmail.com>
2022-09-27 16:20:20 +1000

    added more colours

--- src/theme.js
+++ src/theme.js
@@ -5,8 +5,14 @@ export default {
       monospace: 'Menlo, monospace',
     },
     colors: {
-      text: '#000',
       background: '#F6F2FF',
-      primary: '#33e',
+      
+      primary1: '#240D57',
+      primary2: '#240D57',
+      primary3: '#240D57',
+      primary4: '#240D57',
+
+      neutral1: '#4F4F4F',
+      neutral2: '#828282'
     },
   }
